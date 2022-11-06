4914779d843a57c69fe1631c7592f9f47d5e3b36
k12ish <k4krish@gmail.com>
2022-11-06 13:03:51 +0000

    added edit clipboard script

---
+++ .scripts/nvim-edit-clipboard.sh
@@ -0,0 +1,14 @@
+
+# This script allows the user to modify their clipboard with neovim
+# Set a keybinding to run this script in a terminal emulator: 
+#
+#     alacritty -e ~/.scripts/nvim-edit-clipboard.sh
+
+
+xclip -selection clipboard -o > ~/.scripts/clipboard
+nvim ~/.scripts/clipboard
+xclip ~/.scripts/clipboard  -verbose -selection clipboard && true
+
+# weirdness: xclip is designed to silently operate in the background; this
+# sleep is a bodge. Consult `man xclip` if you wish to sink more time into this
+sleep 0.1
