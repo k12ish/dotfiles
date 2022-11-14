a81da6f8f3ebc37cbb358dc734386863bc988e32
k12ish <k4krish@gmail.com>
2022-11-14 17:36:13 +0000

    added ipython launcher keybinding

---
+++ .scripts/prelude.py
@@ -0,0 +1,7 @@
+# This script allows the user to open ipython with common modules imported
+# Set a keybinding to run this script in a terminal emulator: 
+#
+#     alacritty -e ipython -i /home/krish/.scripts/prelude.py
+#
+
+from dateparser import parse
