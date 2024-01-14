edf86d37a6774e5375e1eccf9a838a6eddf2a3ef
k12ish <k4krish@gmail.com>
2024-01-14 19:30:02 +0000

    omg i forgot what I was missing: ctrl+alt+n to edit clipboard

--- .scripts/edit-clipboard.sh
+++ .scripts/edit-clipboard.sh
@@ -1,14 +1,9 @@
-
-# This script allows the user to modify their clipboard with neovim
+# This script allows the user to modify their clipboard with helix
 # Set a keybinding to run this script in a terminal emulator: 
 #
 #     alacritty -e /home/krish/.scripts/edit-clipboard.sh
 
 
-xclip -selection clipboard -o > ~/.scripts/clipboard
+wl-paste > ~/.scripts/clipboard
 helix ~/.scripts/clipboard
-xclip ~/.scripts/clipboard -selection clipboard && true
-
-# weirdness: xclip is designed to silently operate in the background; this
-# sleep is a bodge. Consult `man xclip` if you wish to sink more time into this
-sleep 0.1
+wl-copy < ~/.scripts/clipboard && true
