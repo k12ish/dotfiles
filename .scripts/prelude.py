b20749c2f4b6d42ce4cf452e0df7e5034d53af73
k12ish <k4krish@gmail.com>
2025-04-27 21:07:38 +0100

    added minor changes

--- .scripts/prelude.py
+++ .scripts/prelude.py
@@ -1,8 +1,12 @@
 # This script allows the user to open ipython with common modules imported
-# Set a keybinding to run this script in a terminal emulator: 
+# Set a keybinding to run this script in a terminal emulator:
 #
-#     alacritty --config-file /home/krish/.config/alacritty/ipython-alacritty.yml -e ipython -i /home/krish/.scripts/prelude.py 
+#     alacritty --config-file /home/krish/.config/alacritty/ipython-alacritty.toml -e ipython -i /home/krish/.scripts/prelude.py
 #
 
-from dateparser import parse
+import itertools
+import math
+from itertools import *
 from math import *
+
+from dateparser import parse
